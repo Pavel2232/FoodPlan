b6bd1d021ef7439102a76bbe453d52e5073b9707
Yaroslav Voroshilov <frqhero@gmail.com>
2023-09-20 23:42:22 +0600

    Add recipe model

--- recipe/models.py
+++ recipe/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Recipe(models.Model):
+    name = models.CharField(max_length=50)
+    DIET_CHOICES = [
+        (1, 'Classic'),
+        (2, 'Low carb'),
+        (3, 'Vegan'),
+        (4, 'Keto'),
+    ]
+    diet = models.IntegerField(choices=DIET_CHOICES)
