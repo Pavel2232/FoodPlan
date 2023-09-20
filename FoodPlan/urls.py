2636e1fcac2584e188452607af4d452984e9b3a5
Pavel2232 <106510078+Pavel2232@users.noreply.github.com>
2023-09-20 16:06:30 +0300

    add privacy_policy

--- FoodPlan/urls.py
+++ FoodPlan/urls.py
@@ -2,7 +2,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
-
+from django.views import defaults as default_views
 from recipe.views import index_view
 
 urlpatterns = [
@@ -13,5 +13,10 @@ urlpatterns = [
 ]
 
 if settings.DEBUG:
+    urlpatterns += (
+        path("403/", default_views.permission_denied, kwargs={"exception": Exception("У вас нет доступа.")}),
+        path("404/", default_views.page_not_found, kwargs={"exception": Exception("Страница не найдена.")}),
+        path("500/", default_views.server_error),
+    )
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
