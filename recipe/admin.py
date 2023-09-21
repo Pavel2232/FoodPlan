4ff3fc5d5cef4f81c4e2c04e37f1ce31f7337e90
Yaroslav Voroshilov <frqhero@gmail.com>
2023-09-21 11:35:18 +0600

    Add last part of models

--- recipe/admin.py
+++ recipe/admin.py
@@ -1,8 +1,23 @@
 from django.contrib import admin
 
-from .models import Recipe
+from .models import Recipe, RecipeIngredient, MeasurementUnit, Ingredient
+
+
+class RecipeIngredientItemInline(admin.TabularInline):
+    model = RecipeIngredient
+    extra = 0
 
 
 @admin.register(Recipe)
 class RecipeAdmin(admin.ModelAdmin):
+    inlines = [RecipeIngredientItemInline]
+
+
+@admin.register(MeasurementUnit)
+class MeasurementUnitAdmin(admin.ModelAdmin):
+    pass
+
+
+@admin.register(Ingredient)
+class IngredientAdmin(admin.ModelAdmin):
     pass
--- recipe/models.py
+++ recipe/models.py
@@ -10,3 +10,30 @@ class Recipe(models.Model):
         (4, 'Keto'),
     ]
     diet = models.IntegerField(choices=DIET_CHOICES)
+    ingredients = models.ManyToManyField(
+        'recipe.Ingredient', through='recipe.RecipeIngredient'
+    )
+
+
+class Ingredient(models.Model):
+    name = models.CharField(max_length=50)
+    calories = models.DecimalField(max_digits=5, decimal_places=2)
+
+    def __str__(self):
+        return f'{self.name}'
+
+
+class MeasurementUnit(models.Model):
+    name = models.CharField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+
+class RecipeIngredient(models.Model):
+    recipe = models.ForeignKey(Recipe, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    amount = models.PositiveIntegerField()
+    measurement_unit = models.ForeignKey(
+        MeasurementUnit, on_delete=models.CASCADE
+    )
