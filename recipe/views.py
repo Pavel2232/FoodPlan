feceb6fcd26e06c73c22ec420b68df3b2fdf454e
Kisly93 <spsvk.kis.sov@gmail.com>
2023-09-24 14:54:38 +0200

    added contacts

--- recipe/views.py
+++ recipe/views.py
@@ -16,3 +16,6 @@ def simple_recipe_view(request):
 
 def detail_recipe_view(request):
     return render(request, 'card2.html')
+
+def contacts_view(request):
+    return render(request, 'contacts.html')
